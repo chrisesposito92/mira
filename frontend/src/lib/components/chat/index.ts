de8811ae2582b8738f98a14da10e168bc940ed48
Chris Esposito <chrisesposito92@gmail.com>
2026-03-02 14:10:06 -0500

    chore: remove unused WorkflowLauncher component

--- frontend/src/lib/components/chat/index.ts
+++ frontend/src/lib/components/chat/index.ts
@@ -5,4 +5,3 @@ export { default as EntityCard } from './EntityCard.svelte';
 export { default as EntityEditDialog } from './EntityEditDialog.svelte';
 export { default as ThinkingIndicator } from './ThinkingIndicator.svelte';
 export { default as WorkflowHeader } from './WorkflowHeader.svelte';
-export { default as WorkflowLauncher } from './WorkflowLauncher.svelte';
